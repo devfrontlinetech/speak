afa8befdded1bcf92b780c35d7b5e073f60ed381
devfrontlinetech <frontlinetech47@gmail.com>
2024-12-14 12:42:13 +0530

    Update footer-social.jsx

--- src/layout/footers/component/footer-social.jsx
+++ src/layout/footers/component/footer-social.jsx
@@ -1,9 +1,9 @@
 const social_share = [
-    { link: 'https://www.facebook.com/frontlinetechin/', target: '_blank', icon: 'icon-facebook', color: 'color-fb' },
-    { link: 'https://twitter.com/FrontlineTechin', target: '_blank', icon: 'icon-twitter', color: 'color-twitter' },
-    { link: 'https://www.linkedin.com/company/frontline-technologies', target: '_blank', icon: 'icon-linkedin2', color: 'color-linkd' },
-    { link: 'https://www.youtube.com/', target: '_blank', icon: 'icon-youtube', color: 'color-yt' },
-    { link: 'https://www.instagram.com/frontlinetechnologies_', target: '_blank', icon: 'icon-instagram', color: 'color-ig' },
+    { link: 'https://www.facebook.com/frontlinetechin/', target: '_blank', icon: 'icon-facebook', color: 'color-fb' aria-label="Follow Frontline Technologies on Facebook" },
+    { link: 'https://twitter.com/FrontlineTechin', target: '_blank', icon: 'icon-twitter', color: 'color-twitter' aria-label="Follow Frontline Technologies on Twitter" },
+    { link: 'https://www.linkedin.com/company/frontline-technologies', target: '_blank', icon: 'icon-linkedin2', color: 'color-linkd' aria-label="Follow Frontline Technologies on Linkedin" },
+    { link: 'https://www.youtube.com/', target: '_blank', icon: 'icon-youtube', color: 'color-yt' aria-label="Follow Frontline Technologies on Youtube" },
+    { link: 'https://www.instagram.com/frontlinetechnologies_', target: '_blank', icon: 'icon-instagram', color: 'color-ig' aria-label="Follow Frontline Technologies on Instagram" },
 ]
 
 import React from 'react';
