643f1dd1289ed6504d187fa3cebb0c0b8d1e7f38
devfrontlinetech <frontlinetech47@gmail.com>
2024-12-17 18:32:47 +0530

    Update header-3.jsx

--- src/layout/headers/header-3.jsx
+++ src/layout/headers/header-3.jsx
@@ -82,7 +82,7 @@ const HeaderThree = () => {
                                     </li>
 
                                     <li className="mobile-menu-bar d-block d-xl-none">
-                                        <button className="hamberger-button" onClick={() => setIsOpen(true)}>
+                                        <button className="hamberger-button" onClick={() => setIsOpen(true)} aria-label="button">
                                             <i className="icon-54"></i>
                                         </button>
                                     </li>
