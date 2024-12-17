cb2fb3a50134157d7c6e42e89e6c8419691178df
devfrontlinetech <frontlinetech47@gmail.com>
2024-12-17 11:34:26 +0530

    Update footer-2.jsx

--- src/layout/footers/footer-2.jsx
+++ src/layout/footers/footer-2.jsx
@@ -139,7 +139,7 @@ const FooterTwo = () => {
                     <div className="row">
                         <div className="col-lg-12">
                             <div className="inner text-center">
-                                <p>Copyright {new Date().getFullYear()} <a href="https://speaksure.in/" rel="noreferrer" target="_blank">Speaksure</a> Designed By <a href="https://frontlinetech.in/" target="_blank" rel="noreferrer">Speaksure</a>. All Rights Reserved</p>
+                                <p>Copyright {new Date().getFullYear()} <a href="https://speaksure.in/" rel="noreferrer" target="_blank" aria-label="Speaksure">Speaksure</a> Designed By <a href="https://frontlinetech.in/" target="_blank" rel="noreferrer" aria-label="Speaksure">Frontline Technologies</a>. All Rights Reserved</p>
                             </div>
                         </div>
                     </div>
