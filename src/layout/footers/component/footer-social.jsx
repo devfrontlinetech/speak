ab29a3164d81f128e9afaa3854aeb3500cb7ba48
devfrontlinetech <frontlinetech47@gmail.com>
2024-12-18 14:52:56 +0530

    Update footer-social.jsx

--- src/layout/footers/component/footer-social.jsx
+++ src/layout/footers/component/footer-social.jsx
@@ -1,9 +1,9 @@
 const social_share = [
-    { link: 'https://www.facebook.com/frontlinetechin/', target: '_blank', icon: 'icon-facebook', color: 'color-fb'  },
-    { link: 'https://twitter.com/FrontlineTechin', target: '_blank', icon: 'icon-twitter', color: 'color-twitter'  },
-    { link: 'https://www.linkedin.com/company/frontline-technologies', target: '_blank', icon: 'icon-linkedin2', color: 'color-linkd'  },
+    { link: 'https://www.facebook.com/frontlinetechin/', target: '_blank', icon: 'icon-facebook', color: 'color-fb'},
+    { link: 'https://twitter.com/FrontlineTechin', target: '_blank', icon: 'icon-twitter', color: 'color-twitter'},
+    { link: 'https://www.linkedin.com/company/frontline-technologies', target: '_blank', icon: 'icon-linkedin2', color: 'color-linkd'},
     { link: 'https://www.youtube.com/', target: '_blank', icon: 'icon-youtube', color: 'color-yt'  },
-    { link: 'https://www.instagram.com/frontlinetechnologies_', target: '_blank', icon: 'icon-instagram', color: 'color-ig'  },
+    { link: 'https://www.instagram.com/frontlinetechnologies_', target: '_blank', icon: 'icon-instagram', color: 'color-ig'},
 ]
 
 import React from 'react';
