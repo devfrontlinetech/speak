e00e1c05fb2cac27d122928b65d39fce2f22fac9
devfrontlinetech <frontlinetech47@gmail.com>
2024-12-28 16:41:21 +0530

    Update courses.jsx

--- src/components/homes/home-online-academy/courses.jsx
+++ src/components/homes/home-online-academy/courses.jsx
@@ -17,7 +17,7 @@ const Courses = () => {
                 </div>
 
                 <div className="row g-5">
-                    {course_data.slice(0,4).map((course) => {
+                    {course_data.slice(0,2).map((course) => {
                         return (
                             <div className="col-xl-6" data-sal-delay="100" data-sal="slide-up" data-sal-duration="800" key={course.id}>
                                 <CourseTypeThree data={course} />
@@ -28,7 +28,7 @@ const Courses = () => {
                 
                 <div className="course-view-all" data-sal-delay="150" data-sal="slide-up" data-sal-duration="1200">
                     <Link href="/" legacyBehavior>
-                        <a className="edu-btn">Browse more courses <i className="icon-4"></i></a>
+                        <a className="our-course">Browse more courses <i className="icon-4"></i></a>
                     </Link>
                 </div>
                 <ul className="shape-group">
