bacf52d6aa58dce95bc8a2530fa240f3153cc8c1
devfrontlinetech <frontlinetech47@gmail.com>
2024-12-30 19:13:50 +0530

    Update about-area.jsx

--- src/components/homes/home-kitchen/about-area.jsx
+++ src/components/homes/home-kitchen/about-area.jsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 import { useMouseMoveUI } from '../../../contexts/mouse-move-context';
 
 const lists = [
-    'Cooking award achived', 
+    'Award achived', 
     'Available online courses', 
     'Online Certification'
 ];
