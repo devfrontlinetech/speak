f380c23214b50c9bf23bc620acb9d8d6423b8636
devfrontlinetech <frontlinetech47@gmail.com>
2024-12-31 10:55:55 +0530

    Update about-area.jsx

--- src/components/homes/home-university/about-area.jsx
+++ src/components/homes/home-university/about-area.jsx
@@ -26,7 +26,7 @@ const tabs = {
         },
         {
             id:'about-mission',
-            desc:" to empower students worldwide to achieve their highest potential on the TOEFL exam. Through personalized instruction, expert-led courses, and a comprehensive approach to language learning, we equip our students with the skills, confidence, and strategies needed to succeed.",
+            desc:" To empower students worldwide to achieve their highest potential on the TOEFL exam. Through personalized instruction, expert-led courses, and a comprehensive approach to language learning, we equip our students with the skills, confidence, and strategies needed to succeed.",
             feature_list:[
                 'Industry Expert Instructor',
                 'Up-to-Date Course Content'
@@ -34,7 +34,7 @@ const tabs = {
         },
         {
             id:'about-vision',
-            desc:"be a global leader in TOEFL coaching, recognized for transforming students language proficiency and enabling them to thrive in academic and professional environments.",
+            desc:"Be a global leader in TOEFL coaching, recognized for transforming students language proficiency and enabling them to thrive in academic and professional environments.",
             feature_list: [
                 'Online Remote Learning',
                 'Lifetime Access For Learning'
