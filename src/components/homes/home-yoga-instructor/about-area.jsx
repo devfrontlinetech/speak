6db60123901e5ce9e23fbed48c63ab1f0e75e4ec
devfrontlinetech <frontlinetech47@gmail.com>
2024-12-31 14:50:24 +0530

    Update about-area.jsx

--- src/components/homes/home-yoga-instructor/about-area.jsx
+++ src/components/homes/home-yoga-instructor/about-area.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import React from 'react';
 
 const lists = [
-    'Cooking award achived', 
+    'Award achived', 
     'Available online courses', 
     'Online Certification'
 ];
@@ -12,8 +12,8 @@ const AboutArea = () => {
         <div className="edu-section-gap edu-about-area about-style-5">
             <div className="container">
                 <div className="section-title section-center" data-sal-delay="150" data-sal="slide-up" data-sal-duration="800">
-                    <span className="pre-title pre-textsecondary">About</span>
-                    <h2 className="title">Hi! I am <span className="color-primary">Simon Baker</span> <br /> and I Offer to develope Your Skill <br /> Through Yoga</h2>
+                    <span className="pre-title pre-textsecondary">Our Commitment</span>
+                    <h2 className="title">Helping You Achieve Your Study <span className="color-primary">Abroad</span> Goals</h2>
                 </div>
                 <div className="row g-5 align-items-center">
                     <div className="col-lg-6">
@@ -31,8 +31,8 @@ const AboutArea = () => {
                     </div>
                     <div className="col-lg-6">
                         <div className="about-content" data-sal-delay="150" data-sal="slide-up" data-sal-duration="800">
-                            <p>Magna aliquaenim minim veniam quis nostrud exercitation ullamco laborisLorem ipsum dolor sit amet consectetur adipisicing elit sed do eius tempor incididunt labore citation</p>
-                            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                            <p> We are passionate about helping students turn their dreams of studying abroad into reality. With years of expertise in navigating the complexities of study visa applications, we are here to provide you with the personalized support and expert guidance you need to succeed.</p>
+                            <p> We aim to create a seamless experience, ensuring that every step of your journey is handled with care and expertise.</p>
                             <ul className="features-list">
                                 {lists.map((l, i) => <li key={i}>{l}</li>)}
                             </ul>
