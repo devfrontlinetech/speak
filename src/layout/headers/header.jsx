f0598d2b7cb3478ed5a57a312dce4a7933f6d35a
devfrontlinetech <frontlinetech47@gmail.com>
2025-01-02 12:10:15 +0530

    Update header.jsx

--- src/layout/headers/header.jsx
+++ src/layout/headers/header.jsx
@@ -141,8 +141,11 @@ const Header = ({ header_style, no_top_bar, disable_full_width, disable_category
                                         )}
                                     </li>
                                     <li className="icon search-icon">
-                                        <button style={{cursor:'pointer;background:transparent;border:none'}} onClick={() => setIsSearchOpen(true)} className="search-trigger">
+{/*                                         <button style={{cursor:'pointer;background:transparent;border:none'}} onClick={() => setIsSearchOpen(true)} className="search-trigger">
                                             <i className="icon-2"></i>
+                                        </button> */}
+                                       <button style={{cursor:'pointer;background:transparent;border:none'}} onClick={() => setIsSearchOpen(true)} className="search-trigger">
+                                            <i className="icon-2" style={{fontSize: "30px";}} ></i>
                                         </button>
                                     </li>
                                     <li className="icon">
