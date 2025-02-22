023098fb09b2f383816b60fa3fbcdedf5595580d
santhosh frontlinetech <santhosh@frontlinetech.in>
2025-02-22 19:12:42 +0530

    modified

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,82 @@
+
 import HomeMain from '../components/homes/home';
 import SEO from '../components/seo';
 import { Wrapper } from '../layout';
+import { NextSeo } from 'next-seo';
+import Head from 'next/head'; // Import Head from next/head
 
 export default function Home() {
     return (
         <Wrapper>
-            <SEO pageTitle={'Home'} />
+            <SEO pageTitle={'Home Main'} />
+            <NextSeo
+                title="Welcome to Speaksure Academy"
+                description="A simple explanation of why coding is so much fun!"
+                openGraph={{
+                    type: 'article',
+                    article: {
+                        publishedTime: '2025-01-27T00:00:00Z',
+                        modifiedTime: '2025-01-27T08:00:00Z',
+                        authors: ['https://myamazingwebsite.com/authors/jane-doe'],
+                        tags: ['Coding', 'Web Development', 'Next.js', 'React.js'],
+                    },
+                }}
+            />
+            <Head>
+                <script
+                    type="application/ld+json"
+                    dangerouslySetInnerHTML={{
+                        __html: JSON.stringify({
+                            '@context': 'https://schema.org',
+                            '@type': 'Article',
+                            headline: 'Welcome to Speaksure Academy',
+                            description: 'A simple explanation of why coding is so much fun!',
+                            datePublished: '2025-01-25T00:00:00Z',
+                            dateModified: '2025-01-25T08:00:00Z',
+                            author: {
+                                '@type': 'Person',
+                                name: 'Anbu',
+                            },
+                            publisher: {
+                                '@type': 'Organization',
+                                name: 'frontlinetraining',
+                                logo: {
+                                    '@type': 'ImageObject',
+                                    url: 'https://myamazingwebsite.com/logo.png',
+                                },
+                            },
+                            image: 'https://myamazingwebsite.com/article-image.jpg',
+                        }),
+                    }}
+                />
+                 <script
+                    type="application/ld+json"
+                    dangerouslySetInnerHTML={{
+                        __html: JSON.stringify({
+                            '@context': 'https://schema.org',
+                            '@type': 'Product',
+                            'name': 'speaksure',
+                            'aggregateRating': {
+                                '@type': 'AggregateRating',
+                                'ratingValue': 4.8, 
+                                'reviewCount': 200,
+                            },    
+                        }),
+                    }}     
+                />
+            </Head>
             <HomeMain />
         </Wrapper>
-    )
-}
+    );
+}   
+
+
+
+
+
+
+
+
+
+
+
