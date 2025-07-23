a0d26b87657061966b372d9923501095a3808559
devfrontlinetech <frontlinetech47@gmail.com>
2025-07-23 16:04:30 +0530

    Update hero-area.jsx

--- src/components/homes/home/hero-area.jsx
+++ src/components/homes/home/hero-area.jsx
@@ -14,7 +14,7 @@ const HeroArea = () => {
   const [selectedTime, setSelectedTime] = useState(null);
 
    // Mock Data
-  const availableDates = [ "22-07-2025", "23-07-2025","26-07-2025", "22-08-2025", "23-08-2025","26-08-2025"];
+  const availableDates = [ "26-07-2025", "22-08-2025", "23-08-2025","26-08-2025"];
   const bookedDates = [ "22-07-2025", "23-07-2025","26-07-2025","27-07-2025"];
   const unavailableDates = [ "06-07-2025", "20-07-2025", "27-07-2025"];
     
