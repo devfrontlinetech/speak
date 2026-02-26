242f77eccea3a59d2b9e44c1d66ae08033024bd4
sumesh <sumesh@frontlinetech.in>
2026-02-26 11:33:43 +0530

    addded

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,17 +1,17 @@
 const defaultSEOConfig = {
-    title: "SpeakSure",
-    description: "Welcome to my example website built with Next.js",
-    openGraph: {
-      type: "website",
-      locale: "en_US",
-      url: "https://speaksure.in",
-      site_name: "speaksure",
-    },
-    twitter: {
-      handle: "@exampletwitterhandle",
-      site: "@exampletwitter",
-      cardType: "summary_large_image",
-    },
-  };
-  
-  export default defaultSEOConfig;
+  title: "SpeakSure",
+  description: "Welcome to my example website built with Next.js",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://speaksure.in",
+    site_name: "speaksure",
+  },
+  twitter: {
+    handle: "@exampletwitterhandle",
+    site: "@exampletwitter",
+    cardType: "summary_large_image",
+  },
+};
+
+export default defaultSEOConfig;
