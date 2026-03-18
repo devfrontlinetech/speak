f2ebc4ef5655a0d6a599d1fe2742d60f1a2f58d6
sumesh <sumesh@frontlinetech.in>
2026-03-18 15:32:28 +0530

    added

--- src/components/english-test/voice-test.jsx
+++ src/components/english-test/voice-test.jsx
@@ -3,24 +3,27 @@ import Link from "next/link";
 
 
 
+
 const VoiceTest = () => {
   const testLink = [
-    { link: 'Practice 20 Questions with "Do" Sentences', path: "/module-1" },
-    { link: 'Practice 20 Questions with "Are" Sentences', path: "/module-2" },
-    { link: "More Modules Coming Soon...", path: "/module-3" },
+    { link: 'Practice 20 Questions with "Do" Sentences', path: "/module-one" },
+    { link: 'Practice 20 Questions with "Are" Sentences', path: "/module-two" },
+    { link: "More Modules Coming Soon...", path: "/module-three" },
   ];
 
   return (
     <div className="voice-container">
+    
+
       <div className="test-right">
         <h1 className="right-head">SpeakTest</h1>
 
         <div className="test-box">
           {testLink.map((item, index) => (
-            <Link key={index} href={item.path}>
+            <Link key={index} to={item.path}>
               <h3 className="test-link">{item.link}</h3>
             </Link>
-          ))}   
+          ))}
         </div>
       </div>
     </div>
