ffd565dce7169bb47a77526ba43d09795b74b3f0
devfrontlinetech <frontlinetech47@gmail.com>
2026-08-13 16:33:12 +0530

    Update hero-area.jsx

--- src/components/homes/home/hero-area.jsx
+++ src/components/homes/home/hero-area.jsx
@@ -19,7 +19,7 @@ const HeroArea = () => {
   const [warningMessage, setWarningMessage] = useState("");
 
   const availableDates = [
-    "2026-08-13",
+  
     "2026-08-15",
     "2026-08-22",
     "2026-08-23",
